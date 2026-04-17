56692abd0323c004efcd6b2ec2a950add9de3e16
RuiGoncalves3 <a29384@alunos.ipca.pt>
2026-04-17 18:52:20 +0100

    feat: figurinos Tarefas 17 (Consultar figurino) e 18 (Criar Anúncio)

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,16 @@ import "dotenv/config";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import protectedRoutes from "./routes/protected.routes.js";
+// ... outros imports
+import figurinoRoutes from './routes/figurino.routes.js';
 
 
 const app = Fastify({ logger: true });
 
 await app.register(cors);
+
+// ... depois dos outros app.use
+app.register(figurinoRoutes, { prefix: '/api/figurinos' });
 
 //TODAS as rotas ficam com /api
 app.register(protectedRoutes, { prefix: "/api" });
@@ -22,3 +27,4 @@ try {
   app.log.error(err);
   process.exit(1);
 }
+
